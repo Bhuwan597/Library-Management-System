376875aea1bc7ba5c6a2f30d04e0e8550bb4a9ef
AsHim Shrestha <ashimsth89@gmail.com>
2024-01-07 18:38:15 +0545

    add seach bar

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+"use client";
+
+import {Button, Input} from "@nextui-org/react";
+import React from "react";
+import {AiOutlineSearch} from "react-icons/ai";
 
 const Hero = () => {
-  return (
-    <div>
-        Hero
-      
-    </div>
-  )
-}
+    const [input, setInput] = React.useState("");
 
-export default Hero
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!input) {
+            return;
+        }
+    }
+
+    return (
+        <form onSubmit={handleSubmit}
+            className='flex w-full mt-5 mx-auto max-w-[90%] border border-gray-200 px-5 py-3 rounded-full hover:shadow-md focus-within:shadow-md transition-shadow sm:max-w-xl lg:max-w-2xl'>
+            <AiOutlineSearch className='text-xl text-gray-500 mr-3 '/>
+            <input type='text' className='flex-grow focus:outline-none' placeholder='Search for Books...' value={input} onChange={(e)=>setInput(e.target.value)}/>
+            </form>
+            );
+        };
+
+export default Hero;
