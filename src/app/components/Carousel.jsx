ada89e9846bab5890331b074fe83cf728b4f1b0d
AsHim Shrestha <ashimsth89@gmail.com>
2024-01-08 14:11:54 +0545

    add : other images in caraousel

--- src/app/components/Carousel.jsx
+++ src/app/components/Carousel.jsx
@@ -79,25 +79,24 @@ const CarouselComponent = () => {
                     altText="Slide 2"
                     displayText={'Motivational Books'}
                 />
-
+                <ImageText
+                    imageUrl="https://images.unsplash.com/photo-1550133730-695473e544be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+                    altText="Slide 3"
+                    displayText={'Explore our Fiction Collection'}
+                />
+                <ImageText
+                    imageUrl="https://images.unsplash.com/photo-1550167164-1b67c2be3973?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+                    altText="Slide 4"
+                    displayText={'Novel Books'}
+                />
+                <ImageText
+                    imageUrl="https://images.unsplash.com/photo-1550338861-b7cfeaf8ffd8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+                    altText="Slide 5"
+                    displayText={'Nepali Books'}
+                />
             </Carousel>
         </div>
 
-        // <img
-        //     src="https://images.unsplash.com/photo-1550133730-695473e544be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
-        //     alt="Slide 3"
-        //
-        // />
-        // <img
-        //     src="https://images.unsplash.com/photo-1550167164-1b67c2be3973?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
-        //     alt="Slide 4"
-        //
-        // />
-        // <img
-        //     src="https://images.unsplash.com/photo-1550338861-b7cfeaf8ffd8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
-        //     alt="Slide 5"
-        //
-        // />
     )
         ;
 }
