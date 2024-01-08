764e15d0b85b2a3b2a74444993ab7333f30fb727
AsHim Shrestha <ashimsth89@gmail.com>
2024-01-08 16:54:45 +0545

    refactor : Input from Next UI

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,17 +1,19 @@
 'use client';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Navbar from "@/app/components/Navbar";
 import Link from "next/link";
-import {Button, Tooltip} from "@nextui-org/react";
+import { Button, Input } from "@nextui-org/react";
 
 const LoginPage = () => {
+
+
     const [formData, setFormData] = useState({
         email: '',
         password: '',
     });
 
     const handleChange = (e) => {
-        const {name, value} = e.target;
+        const { name, value } = e.target;
         setFormData((prevData) => ({
             ...prevData,
             [name]: value,
@@ -25,7 +27,7 @@ const LoginPage = () => {
 
     return (
         <>
-            <Navbar/>
+            <Navbar />
             <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
                     <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-red-600">
@@ -36,34 +38,20 @@ const LoginPage = () => {
                 <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
                     <form className="space-y-6" onSubmit={handleSubmit}>
                         <div>
-                            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                                Email address
-                                <Tooltip
-                                    content="Use your WRC college email address"
-                                    className="ml-1 text-red-600"
-                                >
-                                    <span>*</span>
-                                </Tooltip>
-                            </label>
                             <div className="mt-2">
-                                <input
-                                    id="email"
-                                    name="email"
+                                <Input
+                                    name='email'
+                                    autoComplete
                                     type="email"
-                                    autoComplete="email"
-                                    required
+                                    label="Email"
                                     value={formData.email}
                                     onChange={handleChange}
-                                    className="block w-full rounded-md border-0 py-1.5  shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6 outline-none text-left pl-1"
                                 />
                             </div>
                         </div>
 
                         <div>
                             <div className="flex items-center justify-between">
-                                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                                    Password
-                                </label>
                                 <div className="text-sm">
                                     <Link href="#" className="font-semibold text-red-600 hover:text-red-500">
                                         Forgot password?
@@ -71,22 +59,19 @@ const LoginPage = () => {
                                 </div>
                             </div>
                             <div className="mt-2">
-                                <input
-                                    id="password"
-                                    name="password"
+                                <Input
+                                    name='password'
+                                    autoComplete
                                     type="password"
-                                    autoComplete="current-password"
-                                    required
+                                    label="Password"
                                     value={formData.password}
                                     onChange={handleChange}
-                                    className="block w-full rounded-md border-0 py-1.5  shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6 outline-none text-left pl-1"
                                 />
                             </div>
                         </div>
 
                         <div>
                             <Button
-                                // isLoading
                                 type="submit"
                                 className="flex w-full justify-center rounded-md bg-red-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
                             >
@@ -98,7 +83,7 @@ const LoginPage = () => {
                     <p className="mt-10 text-center text-sm text-gray-500">
                         Don&apos;t Have an Account?{' '}
                         <Link className="font-semibold leading-6 text-red-600 hover:text-red-500" href='/register'>
-                           Sign Up
+                            Sign Up
                         </Link>
                     </p>
                 </div>
--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -2,7 +2,7 @@
 import React, {useState} from 'react';
 import Navbar from "@/app/components/Navbar";
 import Link from "next/link";
-import {Button, Tooltip} from "@nextui-org/react";
+import {Button, Input} from "@nextui-org/react";
 
 const RegisterPage = () => {
     const [formData, setFormData] = useState({
@@ -24,7 +24,6 @@ const RegisterPage = () => {
         e.preventDefault();
         console.log('Registering with:', formData);
 
-        // Add your registration logic here, e.g., making an API call to register the user
     };
 
     return (
@@ -40,87 +39,59 @@ const RegisterPage = () => {
                 <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
                     <form className="space-y-6" onSubmit={handleSubmit}>
                         <div>
-                            <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">
-                                Name
-                            </label>
+
                             <div className="mt-2">
-                                <input
-                                    id="name"
-                                    name="name"
+                                <Input
+                                    name='name'
                                     type="text"
-                                    autoComplete="name"
-                                    required
+                                    label="Name"
                                     value={formData.name}
                                     onChange={handleChange}
-                                    className="block w-full rounded-md border-0 py-1.5  shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6 outline-none text-left pl-1"
                                 />
                             </div>
                         </div>
 
                         <div>
-                            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                                Email address
-                                <Tooltip
-                                    content="Use your WRC college email address"
-                                    className="ml-1 text-red-600"
-                                >
-                                    <span>*</span>
-                                </Tooltip>
-                            </label>
+
                             <div className="mt-2">
-                                <input
-                                    id="email"
-                                    name="email"
+                                <Input
+                                    name='email'
                                     type="email"
-                                    autoComplete="email"
-                                    required
+                                    label="Email"
                                     value={formData.email}
                                     onChange={handleChange}
-                                    className="block w-full rounded-md border-0 py-1.5  shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6 outline-none text-left pl-1"
                                 />
                             </div>
                         </div>
 
                         <div>
-                            <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                                Password
-                            </label>
+
                             <div className="mt-2">
-                                <input
-                                    id="password"
-                                    name="password"
+                                <Input
+                                    name='password'
                                     type="password"
-                                    autoComplete="new-password"
-                                    required
+                                    label="Password"
                                     value={formData.password}
                                     onChange={handleChange}
-                                    className="block w-full rounded-md border-0 py-1.5  shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6 outline-none text-left pl-1"
                                 />
                             </div>
                         </div>
 
                         <div>
-                            <label htmlFor="confirmPassword"
-                                   className="block text-sm font-medium leading-6 text-gray-900">
-                                Confirm Password
-                            </label>
+
                             <div className="mt-2">
-                                <input
-                                    id="confirmPassword"
-                                    name="confirmPassword"
+                                <Input
+                                    name='confirmPassword'
                                     type="password"
-                                    autoComplete="new-password"
-                                    required
+                                    label="Confirm Password"
                                     value={formData.confirmPassword}
                                     onChange={handleChange}
-                                    className="block w-full rounded-md border-0 py-1.5  shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6 outline-none text-left pl-1"
                                 />
                             </div>
                         </div>
 
                         <div>
                             <Button
-                                // isLoading
                                 type="submit"
                                 className="flex w-full justify-center rounded-md bg-red-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
                             >
