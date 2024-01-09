c7ac99ec4b443f81ec96847af9982db4359ac209
AsHim Shrestha <ashimsth89@gmail.com>
2024-01-09 17:57:23 +0545

    LoginForm.jsx completed

--- src/app/components/partials/LoginForm.jsx
+++ src/app/components/partials/LoginForm.jsx
@@ -1,10 +1,91 @@
-import React from 'react';
+import React, {useState} from "react";
+import {Button, Input} from "@nextui-org/react";
+import {FaEnvelope, FaEye, FaEyeSlash} from "react-icons/fa";
 
-const LoginForm = () => {
+const LoginForm = ({onSubmit}) => {
+    const [isVisible, setIsVisible] = React.useState(false);
+
+    const toggleVisibility = () => setIsVisible(!isVisible);
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    });
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        onSubmit(formData);
+    };
+
     return (
-        <div>
-            
-        </div>
+        <form className="space-y-6" onSubmit={handleSubmit}>
+            <div>
+                <div className="mt-2">
+                    <Input
+                        name="email"
+                        autoComplete="email"
+                        type="email"
+                        label="Email"
+                        value={formData.email}
+                        onChange={handleChange}
+                        endContent={<FaEnvelope
+                            className="text-xl text-default-400 pointer-events-none flex-shrink-0"/>}
+                    />
+                </div>
+            </div>
+
+            <div>
+                <div className="flex items-center justify-between">
+                    <div className="text-sm">
+                        <button
+                            type="button"
+                            className="font-semibold text-red-600 hover:text-red-500"
+                        >
+                            Forgot password?
+                        </button>
+                    </div>
+                </div>
+                <div className="mt-2">
+                    <Input
+                        name="password"
+                        autoComplete="current-password"
+                        type={isVisible ? "text" : "password"}
+                        label="Password"
+                        value={formData.password}
+                        onChange={handleChange}
+                        endContent={
+                            <button
+                                className="focus:outline-none"
+                                type="button"
+                                onClick={toggleVisibility}
+                            >
+                                {isVisible ? (
+                                    <FaEyeSlash className="text-2xl text-default-400 pointer-events-none"/>
+                                ) : (
+                                    <FaEye className="text-2xl text-default-400 pointer-events-none"/>
+                                )}
+                            </button>
+                        }
+                    />
+                </div>
+            </div>
+
+            <div>
+                <Button
+                    type="submit"
+                    className="flex w-full justify-center rounded-md bg-red-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
+                >
+                    Sign in
+                </Button>
+            </div>
+        </form>
     );
 };
 
--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,115 +1,41 @@
-"use client";
-import React, { useState } from "react";
+'use client';
+import React from 'react';
 import Navbar from "@/app/components/Navbar";
 import Link from "next/link";
-import { Button, Input } from "@nextui-org/react";
-import { FaEnvelope, FaEye, FaEyeSlash } from "react-icons/fa";
-import Forgotmodal from "../components/partials/Forgotmodal";
+import LoginForm from "@/app/components/partials/LoginForm";
 
 const LoginPage = () => {
-  const [isVisible, setIsVisible] = React.useState(false);
+    const handleLogin = (formData) => {
+        console.log("Logging in with:", formData);
+        // Add your login logic here
+    };
 
-  const toggleVisibility = () => setIsVisible(!isVisible);
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-  });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Logging in with:", formData);
-  };
-
-  return (
-    <>
-      <Navbar />
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-red-600">
-            Sign in to your account
-          </h2>
-        </div>
-
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" onSubmit={handleSubmit}>
-            <div>
-              <div className="mt-2">
-                <Input
-                  name="email"
-                  autoComplete="email"
-                  type="email"
-                  label="Email"
-                  value={formData.email}
-                  onChange={handleChange}
-                  endContent={
-                    <FaEnvelope className="text-xl text-default-400 pointer-events-none flex-shrink-0" />
-                  }
-                />
-              </div>
-            </div>
-
-            <div>
-              <div className="flex items-center justify-between">
-                <div className="text-sm">
-                  <Forgotmodal text={'Forgot Password?'}/>
+    return (
+        <>
+            <Navbar/>
+            <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-red-600">
+                        Sign in to your account
+                    </h2>
                 </div>
-              </div>
-              <div className="mt-2">
-                <Input
-                  name="password"
-                  autoComplete="current-password"
-                  type={isVisible ? "text" : "password"}
-                  label="Password"
-                  value={formData.password}
-                  onChange={handleChange}
-                  endContent={
-                    <button
-                      className="focus:outline-none"
-                      type="button"
-                      onClick={toggleVisibility}
-                    >
-                      {isVisible ? (
-                        <FaEyeSlash className="text-2xl text-default-400 pointer-events-none" />
-                      ) : (
-                        <FaEye className="text-2xl text-default-400 pointer-events-none" />
-                      )}
-                    </button>
-                  }
-                />
-              </div>
-            </div>
 
-            <div>
-              <Button
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-red-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
-              >
-                Sign in
-              </Button>
-            </div>
-          </form>
+                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                    <LoginForm onSubmit={handleLogin}/>
 
-          <p className="mt-10 text-center text-sm text-gray-500">
-            Don&apos;t Have an Account?{" "}
-            <Link
-              className="font-semibold leading-6 text-red-600 hover:text-red-500"
-              href="/register"
-            >
-              Sign Up
-            </Link>
-          </p>
-        </div>
-      </div>
-    </>
-  );
+                    <p className="mt-10 text-center text-sm text-gray-500">
+                        Don&apos;t Have an Account?{" "}
+                        <Link
+                            className="font-semibold leading-6 text-red-600 hover:text-red-500"
+                            href="/register"
+                        >
+                            Sign Up
+                        </Link>
+                    </p>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default LoginPage;
