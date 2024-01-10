265f218908e92a3256575abc8a50d8f44dc66b8d
AsHim Shrestha <ashimsth89@gmail.com>
2024-01-10 17:36:12 +0545

    style : changes in testimonials

--- src/app/components/partials/TestimonialCards.jsx
+++ src/app/components/partials/TestimonialCards.jsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import { Avatar, Card } from "@nextui-org/react";
-import {  FaStar } from 'react-icons/fa';
+import {Avatar, Card} from "@nextui-org/react";
+import {FaStar} from 'react-icons/fa';
 
-const TestimonialCards = ({ name, content, designation, avatarSrc, rating }) => {
+const TestimonialCards = ({name, content, designation, avatarSrc, rating}) => {
     const renderStars = () => {
         const stars = [];
         for (let i = 1; i <= 5; i++) {
             stars.push(
-                <FaStar key={i} className={`text-${i <= rating ? 'yellow-500' : 'gray-400'} mr-1`} />
+                <FaStar key={i} className={`text-${i <= rating ? 'yellow-500' : 'gray-400'} text-xl mr-1`}/>
             );
         }
         return stars;
     };
-    return (
-        <Card isPressable className="max-w-md mx-auto p-6 rounded-md shadow-md bg-gray-50 hover:bg-gray-200 transition-transform transform hover:scale-105">
-            <p className="text-lg text-gray-700 mb-4">
-                {content}
-            </p>
 
-            <div className="flex items-center">
+    return (
+        <Card
+            isPressable
+            className="max-w-md mx-auto p-6 rounded-md shadow-md bg-white hover:shadow-lg transition-transform transform hover:scale-105"
+        >
+            <p className="text-lg text-gray-700 mb-4">{content}</p>
+
+            <div className="flex items-center justify-between">
                 <div className="flex items-center">
-                    <Avatar isBordered radius="lg" src={avatarSrc} />
+                    <Avatar isBordered radius="lg" src={avatarSrc}/>
                     <div className="ml-4">
                         <p className="font-semibold text-indigo-600 text-lg">{name}</p>
                         {designation && <p className="text-gray-600">{designation}</p>}
@@ -29,7 +31,10 @@ const TestimonialCards = ({ name, content, designation, avatarSrc, rating }) => 
 
                 {rating && (
                     <div className="flex items-center ml-4">
-                        {renderStars()}
+                        <div className="flex items-center">
+                            <span className="text-gray-600 mr-1">Rating:</span>
+                            {renderStars()}
+                        </div>
                     </div>
                 )}
             </div>
