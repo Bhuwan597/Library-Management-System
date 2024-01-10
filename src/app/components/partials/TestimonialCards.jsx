3a0f9cf36827917d14d2f1ee0b652f0cd609ea24
AsHim Shrestha <ashimsth89@gmail.com>
2024-01-10 17:53:02 +0545

    testimonials done

--- src/app/components/partials/TestimonialCards.jsx
+++ src/app/components/partials/TestimonialCards.jsx
@@ -1,42 +1,27 @@
 import React from 'react';
 import {Avatar, Card} from "@nextui-org/react";
-import {FaStar} from 'react-icons/fa';
+import Link from "next/link";
 
-const TestimonialCards = ({name, content, designation, avatarSrc, rating}) => {
-    const renderStars = () => {
-        const stars = [];
-        for (let i = 1; i <= 5; i++) {
-            stars.push(
-                <FaStar key={i} className={`text-${i <= rating ? 'yellow-500' : 'gray-400'} text-xl mr-1`}/>
-            );
-        }
-        return stars;
-    };
-
+const TestimonialCards = ({name, content, designation, avatarSrc}) => {
     return (
         <Card
             isPressable
-            className="max-w-md mx-auto p-6 rounded-md shadow-md bg-gray-50 hover:shadow-lg transition-transform transform hover:scale-105"
+            className="max-w-md mx-auto p-6 rounded-md shadow-md bg-gray-50 hover:shadow-lg hover:bg-gray-200 transition-transform transform hover:scale-105"
         >
             <p className="text-lg text-gray-700 mb-4">{content}</p>
 
             <div className="flex items-center justify-between">
                 <div className="flex items-center">
-                    <Avatar isBordered radius="lg" src={avatarSrc}/>
-                    <div className="ml-4">
-                        <p className="font-semibold text-indigo-600 text-lg">{name}</p>
-                        {designation && <p className="text-gray-600">{designation}</p>}
-                    </div>
-                </div>
-
-                {rating && (
-                    <div className="flex items-center ml-4">
+                    <Link target='_blank' href="#" passHref>
                         <div className="flex items-center">
-                            <span className="text-gray-600 mr-1">Rating:</span>
-                            {renderStars()}
+                            <Avatar isBordered radius="lg" src={avatarSrc}/>
+                            <div className="ml-4">
+                                <p className="font-semibold text-indigo-600 text-lg">{name}</p>
+                                {designation && <p className="text-gray-600 text-sm">{designation}</p>}
+                            </div>
                         </div>
-                    </div>
-                )}
+                    </Link>
+                </div>
             </div>
         </Card>
     );
