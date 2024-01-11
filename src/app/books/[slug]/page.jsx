cd447b386bc0549e7af1e7e0c448c61d80b68a55
AsHim Shrestha <ashimsth89@gmail.com>
2024-01-11 21:28:17 +0545

    feat : create book slug dynamic route

---
+++ src/app/books/[slug]/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BookSlugPage = () => {
+    return (
+        <div>
+            Heyy I am a book slug page
+        </div>
+    );
+};
+
+export default BookSlugPage;
