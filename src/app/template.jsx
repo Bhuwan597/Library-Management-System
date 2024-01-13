dea59b9d5a4b878c4d59271ae05a8c194506ce9e
AsHim Shrestha <ashimsth89@gmail.com>
2024-01-13 21:50:57 +0545

    animations : simple transition easeinout

--- src/app/template.jsx
+++ src/app/template.jsx
@@ -1,25 +1,15 @@
-'use client';
+'use client'
 
-import {motion} from 'framer-motion';
-
-const animationVariants = {
-    initial: {opacity: 0, scale: 0.8, translateY: -20},
-    animate: {opacity: 1, scale: 1, translateY: 0},
-    exit: {opacity: 0, scale: 0.8, translateY: -20}
-};
-
-const animationTransition = {ease: 'easeInOut', duration: 0.75, delay: 0.1};
+import {motion} from 'framer-motion'
 
 export default function Template({children}) {
     return (
         <motion.div
-            variants={animationVariants}
-            initial="initial"
-            animate="animate"
-            exit="exit"
-            transition={animationTransition}
+            initial={{opacity: 0}}
+            animate={{opacity: 1}}
+            transition={{ease: 'easeInOut', duration: 0.75}}
         >
             {children}
         </motion.div>
-    );
+    )
 }
