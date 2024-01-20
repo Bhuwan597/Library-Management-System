bc3fb61004cee8e6a96178b39b20f91797bc6b1f
AsHim Shrestha <ashimsth89@gmail.com>
2024-01-20 14:52:13 +0545

    feat : update my books page

--- src/app/data/MyBooksData.js
+++ src/app/data/MyBooksData.js
@@ -2,24 +2,27 @@ export const myBooks = [{
     title: "PhirPhire",
     image_url: "/phirphire.jpg",
     author: "BuddhiSagar",
-    borrowDate: "2024-01-05",
+    borrowDate: "2024-01-15",
+    requested: false,
 
 }, {
     title: "Jhola",
     image_url: "/jhola.jpg",
     author: "Krishna Dharabasi",
     borrowDate: "2024-01-08",
+    requested: true,
 
 }, {
     title: "The Power of Positive Thinking",
     image_url: "/thepowerofpositivethinking.jpg",
     author: "Norman Vincent Peale",
     borrowDate: "2024-01-02",
+    requested: false,
 }, {
     title: "Summer Love",
     image_url: "/summerlove.jpg",
     author: "Subin Bhattarai",
-    borrowDate: "2024-01-19",
+    borrowDate: "2024-01-19", requested: true,
 },
 
 ]
