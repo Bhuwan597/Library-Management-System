175f59b643b5555b92a5a3c1f59018000608bf9d
AsHim Shrestha <ashimsth89@gmail.com>
2024-01-24 20:19:24 +0545

    style :  all users table

--- src/app/admin/users/page.jsx
+++ src/app/admin/users/page.jsx
@@ -28,6 +28,26 @@ const UserData = [
         username: "jane_doe",
         email: "jane@example.com",
         dateCreated: "2024-01-25",
+    }, {
+        id: 3,
+        username: "jane_doe",
+        email: "jane@example.com",
+        dateCreated: "2024-01-25",
+    }, {
+        id: 4,
+        username: "jane_doe",
+        email: "jane@example.com",
+        dateCreated: "2024-01-25",
+    }, {
+        id: 5,
+        username: "jane_doe",
+        email: "jane@example.com",
+        dateCreated: "2024-01-25",
+    }, {
+        id: 6,
+        username: "jane_doe",
+        email: "jane@example.com",
+        dateCreated: "2024-01-25",
     },
 
 ];
@@ -51,100 +71,103 @@ const UserTable = () => {
 
     return (
         <>
-            <Modal
-                placement={"center"}
-                isOpen={!!selectedUser}
-                onOpenChange={closeModal}
-                size="xl"
-            >
-                <ModalContent className="bg-white p-8 rounded-lg shadow-lg">
-                    {selectedUser && (
-                        <>
-                            <ModalHeader
-                                className="text-center text-4xl font-extrabold mb-4 border-b pb-2 text-gray-800">
-                                <div className="flex items-center">
-                                    <FaUserAlt size="50" className="text-3xl mr-2 text-indigo-600"/>
-                                    <span>{selectedUser.username}</span>
-                                </div>
-                            </ModalHeader>
-                            <ModalBody className="mb-4 text-gray-800">
-                                <div className="mt-4 space-y-4">
-                                    <p className="text-lg text-center">
-                                        Do you want to delete this user?
-                                    </p>
-                                </div>
-                            </ModalBody>
-                            <ModalFooter className="flex justify-center mt-4">
-                                <Button
-                                    color="danger"
-                                    variant="solid"
-                                    onPress={deleteUser}
-                                    className="px-6 py-2 bg-red-500 text-white hover:bg-red-600 transition flex items-center rounded-full"
-                                >
-                                    <FaTrash className="text-xl mr-2"/>
-                                    Delete User
-                                </Button>
-                                <Button
-                                    onPress={closeModal}
-                                    className="px-6 py-2 bg-gray-300 text-white hover:bg-gray-400 transition flex items-center rounded-full ml-4"
-                                >
-                                    Cancel
-                                </Button>
-                            </ModalFooter>
-                        </>
-                    )}
-                </ModalContent>
-            </Modal>
 
-            <Table className="min-w-full bg-white border border-gray-300">
-                <TableHeader className="bg-gray-100">
-                    <TableColumn className="py-3 px-4 text-lg font-semibold">
-                        Users
-                    </TableColumn>
-                    <TableColumn className="py-3 px-4 text-lg font-semibold text-center">
-                        Email
-                    </TableColumn>
-                    <TableColumn className="py-3 px-4 text-lg font-semibold text-center">
-                        Date Created
-                    </TableColumn>
-                    <TableColumn className="py-3 px-4 text-lg font-semibold text-center">
-                        Actions
-                    </TableColumn>
-                </TableHeader>
+                <Modal
+                    placement={"center"}
+                    isOpen={!!selectedUser}
+                    onOpenChange={closeModal}
+                    size={'sm'}
+                >
+                    <ModalContent className="bg-white p-8 rounded-lg shadow-lg">
+                        {selectedUser && (
+                            <>
+                                <ModalHeader
+                                    className="text-center text-xl font-extrabold  border-b text-gray-800">
+                                    <div className="flex items-center">
+                                        <FaUserAlt size="30" className="mr-2 text-gray-700"/>
+                                        <span>{selectedUser.username}</span>
+                                    </div>
+                                </ModalHeader>
+                                <ModalBody className=" text-gray-800">
+                                    <div>
+                                        <p className="text-lg text-center">
+                                            Do you want to delete this user?
+                                        </p>
+                                    </div>
+                                </ModalBody>
+                                <ModalFooter className="flex justify-center">
+                                    <Button
+                                        color="danger"
+                                        variant="solid"
+                                        onPress={deleteUser}
+                                        className="px-6 py-2 bg-red-500 text-white hover:bg-red-600 transition flex items-center rounded-full"
+                                    >
+                                        <FaTrash className="text-xl animate-pulse mr-2"/>
+                                        Delete User
+                                    </Button>
+                                    <Button
+                                        onPress={closeModal}
+                                        className="px-6 py-2 bg-gray-500 text-white hover:bg-gray-400 transition flex items-center rounded-full ml-2"
+                                    >
+                                        Cancel
+                                    </Button>
+                                </ModalFooter>
+                            </>
+                        )}
+                    </ModalContent>
+                </Modal>
 
-                <TableBody emptyContent={"No Users to Display!!!"}>
-                    {UserData.map((user) => (
-                        <TableRow
-                            key={user.id}
-                            className="hover:bg-gray-50 transition duration-300 ease-in-out"
-                        >
-                            <TableCell className="py-3 px-4">
-                                <div className="flex items-center">
-                                    <p className={`text-sm font-medium sm:text-lg `}>
-                                        {user.username}
-                                    </p>
-                                </div>
-                            </TableCell>
-                            <TableCell className="py-3 px-4 text-lg text-center">
-                                {user.email}
-                            </TableCell>
-                            <TableCell className="py-3 px-4 text-lg text-center">
-                                {user.dateCreated}
-                            </TableCell>
-                            <TableCell className="py-3 px-4 text-center">
-                                <Button
-                                    isIconOnly
-                                    size="sm"
-                                    variant="light"
-                                    onClick={() => openModal(user)}
-                                >
-                                    <FaTrash size={"20"} className="text-gray-400"/>
-                                </Button>
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
+                <Table className="min-w-full bg-white border border-gray-300">
+                    <TableHeader className="bg-gray-100">
+                        <TableColumn className=" text-lg font-semibold text-center">
+                            Users
+                        </TableColumn>
+                        <TableColumn className="text-lg font-semibold text-center">
+                            Email
+                        </TableColumn>
+                        <TableColumn className="text-lg font-semibold text-center">
+                            Date Created
+                        </TableColumn>
+                        <TableColumn className="text-lg font-semibold text-center">
+                            Delete
+                        </TableColumn>
+                    </TableHeader>
+
+                    <TableBody emptyContent={"No Users to Display!!!"}>
+                        {UserData.map((user) => (
+                            <TableRow
+                                key={user.id}
+                                className="hover:bg-gray-50 transition duration-300 ease-in-out"
+                            >
+                                <TableCell className="py-3 px-4">
+                                    <div className="flex items-center">
+                                        <p className={`text-sm font-medium sm:text-lg `}>
+                                            {user.username}
+                                        </p>
+                                    </div>
+                                </TableCell>
+                                <TableCell className=" text-sm text-center sm:text-lg">
+                                    {user.email}
+                                </TableCell>
+                                <TableCell className=" text-sm text-center sm:text-lg">
+                                    {user.dateCreated}
+                                </TableCell>
+                                <TableCell className="py-3 px-4 text-center sm:text-lg">
+                                    <Button
+                                        isIconOnly
+                                        size="sm"
+                                        variant="light"
+                                        className={'text-red'}
+                                        onClick={() => openModal(user)}
+                                    >
+                                        <FaTrash size={"20"} className="text-red-600"/>
+                                    </Button>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+
         </>
     );
 };
